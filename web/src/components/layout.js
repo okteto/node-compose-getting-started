50f1212ee8a7ef7e4cc4c08bc4658d1b90283548
Ramiro Berrelleza <ramiro@okteto.com>
2022-02-09 14:31:54 -0800

    use build args on the web

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -7,7 +7,7 @@ export default class Layout extends Component {
       <div>
         <nav class="navbar navbar-expand navbar-dark bg-dark">
         <a class="nav-link navbar-brand" href="/"><img src="okteto.png" width="36px" height="36px" /></a>
-          <a class="nav-link navbar-brand" href="/">My Movie List</a>
+          <a class="nav-link navbar-brand" href="/">{process.env.REACT_APP_TITLE}</a>
           <a class="nav-link navbar-brand" href="add">New Movie</a>
         </nav>
         <div class="container mt-3">
